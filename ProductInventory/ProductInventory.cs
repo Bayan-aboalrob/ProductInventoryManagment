15d4348de7e1a1174407a8bffb6c13bf11712759
Bayan_2001 <bayanaboalrob2001@gmail.com>
2024-04-06 21:36:24 +0300

    Editing AddProduct and EditProductDetails

--- ProductInventory/ProductInventory.cs
+++ ProductInventory/ProductInventory.cs
@@ -42,7 +42,70 @@ namespace ProductInventory
             Console.WriteLine("Product added successfully !");
         }
         //Edit method for viewing a product modification options
-        
+
+        public static void EditMenu()
+        {
+            Console.WriteLine("Please choose one of the following option");
+            Console.WriteLine("******************************************");
+            Console.WriteLine("1. Edit Quantity");
+            Console.WriteLine("2. Edit Price");
+
+        }
+
+        // This a method for editing a product details based on the user choice: whether to edit
+        // the price or the quantity
+        public static void EditProduct(ProductInventory inventory)
+        {
+            Console.WriteLine("Please enter the name of the product you want to edit:");
+            string productName = Console.ReadLine();
+
+            Product product = inventory.GetProducts().Find(p => p.Name.ToLower() == productName.ToLower());
+
+            if (product != null)
+            {
+                EditMenu();
+                string editOption = Console.ReadLine();
+                int convertedEditOption;
+
+                if (int.TryParse(editOption, out convertedEditOption))
+                {
+                    switch (convertedEditOption)
+                    {
+                        case 1:
+                            Console.WriteLine("Please enter the new quantity of the product:");
+                            int newQuantity;
+                            while (!int.TryParse(Console.ReadLine(), out newQuantity))
+                            {
+                                Console.WriteLine("Invalid quantity. Please enter a valid number:");
+                            }
+                            product.Quantity = newQuantity;
+                            Console.WriteLine("Quantity updated successfully !");
+                            break;
+                        case 2:
+                            Console.WriteLine("Please enter the new price of the product:");
+                            double newPrice;
+                            while (!double.TryParse(Console.ReadLine(), out newPrice))
+                            {
+                                Console.WriteLine("Invalid price. Please enter a valid number:");
+                            }
+                            product.Price = newPrice;
+                            Console.WriteLine("Price updated successfully !");
+                            break;
+                        default:
+                            Console.WriteLine("Invalid option.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid option.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Product not found.");
+            }
+        }
 
 
     }
